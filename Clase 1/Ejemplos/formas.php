24f1b00faa14aecff528dd917cc183c23db58257
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2019-04-26 22:25:14 -0500

    Agrega ejemplo de formas

---
+++ Clase 1/Ejemplos/formas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forma de Prueba</title>
+</head>
+<body>
+    <h1>Gracias por ayudarnos con las pruebas :D!</h1>
+    <p>La información que enviaste fue la siguiente:</p>
+    <ul>
+        <li><strong>Nombre:</strong> <?php echo $_POST['nombre']?></li>
+        <li><strong>Email:</strong> <?php echo $_POST['email']?></li>
+        <li><strong>Asunto:</strong> <?php echo $_POST['asunto']?></li>
+        <li><strong>Mensaje:</strong> <?php echo $_POST['mensaje']?></li>
+    </ul>
+</body>
+</html>
