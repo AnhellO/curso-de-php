43c432ab92c86ae3f85df6cdba5e304b295579c8
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2019-04-26 19:21:27 -0500

    Agregando ejemplo de scope

---
+++ Clase 1/Ejemplos/scope.php
@@ -0,0 +1,35 @@
+<?php
+
+// Probando scope local
+$var1 = 'var1!';
+echo "{$var1}\n";
+
+// Probando scope global
+function testScopeGlobal() {
+	// Variable global
+	global $var2;
+	$var2 = 'var2!';
+
+	// Variable de scope local
+	$var3 = 'var3!';
+}
+
+testScopeGlobal();
+echo "{$var1}\n";
+echo "{$var2}\n";
+echo "{$var3}\n";
+
+// Probando scope estático
+function testScopeEstatico() {
+	// Variable global
+	static $var4 = 'var4!';
+	echo "{$var4}\n";
+	$var4 .= '!';
+}
+
+// var4!
+echo testScopeEstatico();
+// var4!!
+echo testScopeEstatico();
+// var4!!!
+echo testScopeEstatico();
